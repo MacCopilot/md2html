afa335d3c21f7335270c789ac2f1281092b83092
zz <wangxili@tsingj.com>
2024-01-02 14:23:38 +0800

    upd

--- wiki_frontend/app/[lang]/docs/layout.tsx
+++ wiki_frontend/app/[lang]/docs/layout.tsx
@@ -1,12 +1,16 @@
 import SideBarWrapper from "@/components/sidebars/SideBarWrapper";
-import LoadingSideBar from "@/components/sidebars/LoadingSideBar"
+import LoadingSideBar from "@/components/sidebars/LoadingSideBar";
 import { Suspense } from "react";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="pt-16 min-h-screen">
-      <Suspense fallback={<LoadingSideBar/>}>
-        <SideBarWrapper isSmall={true} />
-        <SideBarWrapper isSmall={false} />
+      <Suspense fallback={<LoadingSideBar />}>
+        <div className="xl:hidden">
+          <SideBarWrapper isSmall={true} />
+        </div>
+        <div className="hidden xl:block">
+          <SideBarWrapper isSmall={false} />
+        </div>
       </Suspense>
       {children}
     </div>
